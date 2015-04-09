2613426301292a2fe210900afe7b59771d055ed2
Chris Hall <chris@captainu.com>
2015-04-08 20:10:08 -0400

    remove spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,11 +5,12 @@ RSpec.describe Post, type: :model do
   let(:user) { User.create(first_name: "test", last_name: "user", email: "test@example.com") }
   let(:existing_post) { Post.create(user: user, title: "title", body: "body") }
 
-  it "does not allow posts for a user to have the same title" do
-    expect{Post.create(user: user, title: "title", body: "body")}.to_not change{Post.count}
+  it "requires a user, title and body" do
   end
 
   it "does not have a body longer than 100 characters" do
+  end
 
+  it "does not allow posts for a user to have the same title" do
   end
 end
